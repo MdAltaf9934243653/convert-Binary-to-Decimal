051729534f07030ca4086282b8282dd0105727c4
Md Altaf <124495033+MdAltaf9934243653@users.noreply.github.com>
2025-10-07 13:37:53 +0530

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,22 @@
+// Code of BinaryT o Decimal
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter a  Number:";
+    cin>>n;
+    int i=0;
+    int ans=0;
+    while(n!=0){
+        int digit=n%10;
+        if(digit==1){
+            ans=ans+pow(2, i);
+            
+        }
+        n=n/10;
+        i++;
+    
+    }
+    cout<<"Answer is:"<<ans<<endl;
+}
